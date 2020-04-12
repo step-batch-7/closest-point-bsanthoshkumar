9312a49dae8ac4a3c0684e60457edd00cdab84ad
santhosh <santhosh.kumar@thoughtworks.com>
2020-04-12 21:36:56 +0530

    implemented get_closest_food function and added distance_between_points function

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "point.h"
+#include <stdio.h>
 
 int main(void)
 {
+  Point food_locations[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+  Point current_location = {36,97};
+  Point closest_food_location;
+  get_closest_food(food_locations,5,current_location,&closest_food_location);
+  printf("Location of organism: [%d %d],  Closest food target : [%d %d]",current_location.x,current_location.y,closest_food_location.x,closest_food_location.y);
+
   return 0;
 }
--- point.c
+++ point.c
@@ -1,4 +1,21 @@
 #include "point.h"
+#include <math.h>
+
+double distance_between_points(Point a,Point b)
+{
+  return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
+}
 
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location) {
+  double length = distance_between_points(current_location, *closest_food_location);
+  for(int index = 0;index < points_length;index++)
+  {
+    double length_to_currentfood = distance_between_points(current_location, food_points[index]);
+    if(length_to_currentfood < length)
+    {
+      length = length_to_currentfood;
+      closest_food_location->x = food_points[index].x;
+      closest_food_location->y = food_points[index].y;
+    }
+  }
 }
